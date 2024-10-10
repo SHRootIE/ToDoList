b805b5dd2bb273bec6135ebf00a02f890c5363e4
Ilya <ilya-cra-sh@yandex.ru>
2024-10-11 00:28:15 +0300

    First commit

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Ilya Shirokov on 11.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
